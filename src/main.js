2e1cd93c5774c3df07bc1d8686527ee13ab386cc
Youngjae Choi <cdw0807@naver.com>
2022-04-13 00:36:06 +0900

    22.04.13

--- src/main.js
+++ src/main.js
@@ -22,10 +22,55 @@ function createHTMLString(item) {
     `;
 }
 
+// 버튼을 클릭할때마다 데이터가 업데이트가 되어버림 => 해결방안 강구
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+
+    // object는 배열처럼 key를 이용하여 데이터에 접근할 수 있다.
+    displayItems(items.filters((item) => item[key] == value));
+}
+
+function onButtonClick2(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+
+    updateItems(items, key, value);
+}
+
+function updateItems(items, key, value) {
+    items.forEach((item) => {
+        if (item.dataset[key] === value) {
+            item.classList.remove("invisible");
+        } else {
+            item.classList.add("invisible");
+        }
+    });
+}
+
+function setEventListners(items) {
+    // 하나하나의 이벤트 리스너를 반복해서 등록하는것 보단 컨테이너에 이벤트 리스너를 등록해서 한곳에서만 핸들링 할 수 있도록 한다.
+    const logo = document.querySelector(".logo");
+    const buttons = document.querySelector(".buttons");
+
+    logo.addEventListener("click", () => displayItems(items));
+    buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 // main
 loadItems()
     .then((items) => {
         displayItems(items);
-        //setEventListners(items);
+        setEventListners(items);
     })
     .catch(console.log);
